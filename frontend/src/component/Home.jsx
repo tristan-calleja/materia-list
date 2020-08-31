45c0da83d063abbbc3e656e6ade929d127568cf3
Tristan Calleja <tristancalleja@Tristans-MacBook-Pro.local>
2020-09-01 00:23:59 +0800

    styling homepage

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, CardColumns } from "react-bootstrap";
+import { Container, Card, CardColumns } from "react-bootstrap";
+import budget from '../images/budget.png';
+import wine from '../images/wine.png';
+// import cheaney from '../images/Cheaney.png';
+import clothing from '../images/clothing.png';
+import watch from '../images/MKII.png';
 
 export default class Home extends Component {
     render() {
@@ -7,60 +12,64 @@ export default class Home extends Component {
                 <Container className="homepage mt-5">
                     <CardColumns>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img variant="top" src={budget} />
                             <Card.Body>
-                            <Card.Title>Card title that wraps to a new line</Card.Title>
+                            <Card.Title>My Budget</Card.Title>
                             <Card.Text>
-                                This is a longer card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                                This will be the budget section
                             </Card.Text>
                             </Card.Body>
                         </Card>
                         <Card className="p-3">
                             <blockquote className="blockquote mb-0 card-body">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
+                                “La perfection est atteinte, non pas lorsqu'il n'y a plus rien à ajouter, mais lorsqu'il n'y a plus rien à retirer.”
                             </p>
                             <footer className="blockquote-footer">
                                 <small className="text-muted">
-                                Someone famous in <cite title="Source Title">Source Title</cite>
+                                <cite title="Source Title">Antoine de Saint Exupéry</cite>
                                 </small>
                             </footer>
                             </blockquote>
                         </Card>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img variant="top" src={wine} />
                             <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title>Wines</Card.Title>
                             <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-                                content.{' '}
+                                This will be the budget section
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src={clothing} />
+                            <Card.Body>
+                            <Card.Title>Clothing</Card.Title>
+                            <Card.Text>
+                                This will be the clothing section.{' '}
                             </Card.Text>
                             </Card.Body>
                             <Card.Footer>
                             <small className="text-muted">Last updated 3 mins ago</small>
                             </Card.Footer>
                         </Card>
-                        <Card bg="primary" text="white" className="text-center p-3">
+                        <Card className="text-right">
                             <blockquote className="blockquote mb-0 card-body">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
+                            “If people don’t love or hate, the result of your work, then you haven’t done all that much.” 
                             </p>
                             <footer className="blockquote-footer">
                                 <small className="text-muted">
-                                Someone famous in <cite title="Source Title">Source Title</cite>
+                                <cite title="Source Title">Tinker Hatfield</cite>
                                 </small>
                             </footer>
                             </blockquote>
                         </Card>
                         <Card className="text-center">
                             <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title>Watches</Card.Title>
                             <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-                                content.{' '}
+                                This will be the watch section.{' '}
                             </Card.Text>
                             <Card.Text>
                                 <small className="text-muted">Last updated 3 mins ago</small>
@@ -68,28 +77,25 @@ export default class Home extends Component {
                             </Card.Body>
                         </Card>
                         <Card>
-                            <Card.Img src="holder.js/100px160" />
+                            <Card.Img src={watch} />
                         </Card>
                         <Card className="text-right">
                             <blockquote className="blockquote mb-0 card-body">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
+                            “And those who were seen dancing were thought to be insane by those who could not hear the music.”
                             </p>
                             <footer className="blockquote-footer">
                                 <small className="text-muted">
-                                Someone famous in <cite title="Source Title">Source Title</cite>
+                                <cite title="Source Title">Frederick Nietzsche</cite>
                                 </small>
                             </footer>
                             </blockquote>
                         </Card>
                         <Card>
                             <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title>Shoes</Card.Title>
                             <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This card has even longer content than the first to
-                                show that equal height action.
+                                This will be the shoes section
                             </Card.Text>
                             <Card.Text>
                                 <small className="text-muted">Last updated 3 mins ago</small>
