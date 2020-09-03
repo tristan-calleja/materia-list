94c80638634c5c74f4596bdda12e037b8a7c2dab
Tristan Calleja <tristancalleja@Tristans-MacBook-Pro.local>
2020-09-03 17:55:13 +0800

    heroku

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");//Heroku deployment 
 
 //=== Middlewares
 require("./config/db"); //calls my mongoose connection to cleanup this file
 app.use(express.json()); //allows me to receive JSON files from HEADER of REQUEST
 app.use(cors()); //allows all requests from outside servers or apps
+app.use(express.static(path.join(__dirname, "frontend", "build"))) //Heroku deployment 
 
 //=== Routes
 app.use("/api/clothings", require("./routes/clothing.route"));
 app.use("/api/auth", require("./routes/auth.route"));
 
 //=== 404 errors
+// app.get("*", (req, res) => {
+//   res.status(404).json({ message: "Are you lost?", code: "EB404" });
+// });
+
+//Heroku deployment 
 app.get("*", (req, res) => {
-  res.status(404).json({ message: "Are you lost?", code: "EB404" });
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 //=== Server PORT
