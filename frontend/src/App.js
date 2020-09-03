0ca2ce4817e7e732b92ddabc7a179a468f06d61f
Tristan Calleja <tristancalleja@Tristans-MacBook-Pro.local>
2020-09-03 18:14:54 +0800

    Add a log

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -86,6 +86,7 @@ export default class App extends Component {
 
   registerHandler = (credentials) => {
     //login here
+    console.log(URL);
     Axios.post(`${URL}/auth/register`, credentials)
       .then((res) => {
         console.log(res.data);
